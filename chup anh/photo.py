394661e0f7a3ec6abbe10b1917320996c74366da
nguyendangnhan1609 <nguyendangnhan1609@gmail.com>
2025-08-01 09:15:04 +0700

    1/8/2025

--- chup anh/photo.py
+++ chup anh/photo.py
@@ -1,1 +1,21 @@
-n = map(int,input().split())
+n = int(input())
+a = [(0,0)]
+for i in range (n):
+   x,y = map(int,input().split())
+   a.append((x,y));
+a = a+[(0,0)]
+c = [0]*4
+i = j = T =1
+lmin = int(1e5)
+c[a[1][1]] = 1
+while i<=j and j<=n:
+    if T<3:
+        j += 1
+        if c[a[j][1]] == 0: T += 1
+        c[a[j][1]] += 1
+    else:
+        lmin = min(lmin , a[j][0]-a[i][0])
+        c[a[i][1]] -= 1
+        if c[a[i][1]] == 0: T -= 1
+        i += 1
+print(lmin)
---
+++ xao bai/card.py
@@ -0,0 +1,3 @@
+n,k = map(int,input().split())
+L = [0]*(n+3)
+R = [0]*(n+3)
